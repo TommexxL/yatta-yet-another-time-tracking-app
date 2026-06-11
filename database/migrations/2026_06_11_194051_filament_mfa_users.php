adf86e64e1818d203bfd97b789fadade7a65e2cf
TommexxL <tommexx_007@hotmail.com>
2026-06-11 22:10:44 +0200

    fix migration filemant MFA users

--- database/migrations/2026_06_11_194051_filament_mfa_users.php
+++ database/migrations/2026_06_11_194051_filament_mfa_users.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->text('app_authentication_secret')->nullable();
             $table->text('app_authentication_recovery_codes')->nullable();
         });

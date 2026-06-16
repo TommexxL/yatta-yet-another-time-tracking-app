a12995543027d8bbefff471244983f267bff505f
TommexxL <tommexx_007@hotmail.com>
2026-06-16 21:55:46 +0200

    added all routes for viewing schedule and time entry corrections

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,18 @@ Route::get('/profile', function () {
 
 
 Route::middleware('auth')->group(function () {
+    Route::get('/schedule', [TimeEntryController::class, 'schedule'])
+        ->name('schedule');
+
     Route::post('/clock-in', [TimeEntryController::class, 'clockIn'])
         ->name('clock-in');
 
     Route::post('/clock-out', [TimeEntryController::class, 'clockOut'])
         ->name('clock-out');
+
+    Route::get('/time-entry-corrections/{date}/create', [TimeEntryController::class, 'createCorrection'])
+        ->name('time-entry.correction.create');
+
+    Route::post('/time-entry-corrections/{date}', [TimeEntryController::class, 'storeCorrection'])
+        ->name('time-entry.correction.store');
 });

415235cf519b729f08c52d9a1ea968295fedad81
TommexxL <tommexx_007@hotmail.com>
2026-06-16 21:53:05 +0200

    adding time entry correction relations to other models

--- app/Models/TimeEntry.php
+++ app/Models/TimeEntry.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable([
     'company_id',
@@ -30,6 +31,11 @@ class TimeEntry extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function corrections(): HasMany
+    {
+        return $this->hasMany(TimeEntryCorrection::class);
     }
 
     /**

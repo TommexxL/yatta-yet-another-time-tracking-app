22fae93a1594ea943740189befa29c350b16daaf
TommexxL <tommexx_007@hotmail.com>
2026-06-22 21:24:52 +0200

    audit security, nobody deletes or edits

--- app/Filament/Resources/AuditLogs/Pages/ViewAuditLog.php
+++ app/Filament/Resources/AuditLogs/Pages/ViewAuditLog.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\AuditLogs\Pages;
 
 use App\Filament\Resources\AuditLogs\AuditLogResource;
-use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewAuditLog extends ViewRecord
 {
     protected static string $resource = AuditLogResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            EditAction::make(),
-        ];
-    }
 }
